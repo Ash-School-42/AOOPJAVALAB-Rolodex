968ca6d6e479431e983a30cd910ad35596734b83
Ash-School-42 <jamash0376@students.ecpi.edu>
2026-04-14 10:53:29 -0400

    Create Project Week 2 Displayable.java

---
+++ Project Week 2 Displayable.java
@@ -0,0 +1,12 @@
+/**
+ * Name: James Ash
+ * Date: 04-14-2026
+ * Assignment: 2.2 Week 2 Project
+ * 
+ * Interface that requires a display method
+ **/
+public interface Displayable {
+
+    // Method all contacts must implement
+    void display();
+}
